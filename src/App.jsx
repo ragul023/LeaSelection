2bb4ff9fd4968e68836e806c50cae7364eeb6d33
ragul023 <ragul4172@gmail.com>
2026-01-14 11:35:46 +0530

    Created Home

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import { BrowserRouter, Routes, Route } from "react-router";
 import Registration from "./pages/Registration";
+import Home from "./pages/Home";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Registration />} />
+        <Route path="/register" element={<Registration />} />
+        <Route path="/home" element={<Home/>}/>
+        <Route path="/" element={<Home/>}/>
       </Routes>
     </BrowserRouter>
   );
